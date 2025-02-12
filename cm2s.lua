76d7004e693587d4e15e486814aba0c03b20e942
Hawkking-cloud <59718035+Hawkking-cloud@users.noreply.github.com>
2025-02-11 22:41:35 -0600

    beautified code

--- cm2s.lua
+++ cm2s.lua
@@ -1,49 +1,72 @@
+local cm2Lua = require("OPTcm2Lua")
 
-local cm2Lua=require("OPTcm2Lua")
 local cm2s = {}
--- this is probably just gonna be a wrapper that takes all the circuits and merges them into one 
-local zI=0
-local masterSave=cm2Lua.new(0,0)
+
+-- this is probably just gonna be a wrapper that takes all the circuits and merges them into one
+
+local zI = 0 --this increments and tells the structure what zindex to use
+
+local masterSave = cm2Lua.new(0, 0) --save that is written to when structures are added, this file mainly just wraps all the structures together
+
 function cm2s:initStruct(structure)
-    structure.STRUCTZ=zI
-    zI=zI+1
+    structure.STRUCTZ = zI
+    zI = zI + 1
     return structure
 end
-local masterBID=1 -- keeps track of the stacked block ids 
-local structureOrigins={}
+
+local masterBID = 1 -- keeps track of the stacked block ids
+local structureOrigins = {} -- had to do some wizardry for this and keeps track of each structures local bid in its time when it was generated
+
 function cm2s:add(structure)
-    masterSave:allocateBlocks(structure._bid-2)
-    masterSave:allocateConnections(structure._cid-2)
-    structureOrigins[structure._hash]=masterSave._bid
-    for i=1,structure._bid-1 do
+    --idfk why i did -2 to each, it just works
+    masterSave:allocateBlocks(structure._bid - 2)
+    masterSave:allocateConnections(structure._cid - 2)
+
+    --wizardry i did to get connections working
+    structureOrigins[structure._hash] = masterSave._bid
+
+    for i = 1, structure._bid - 1 do
         masterSave:addBlockRaw(structure._blocks[i])
     end
-    for i=1,structure._cid-1 do
+
+    for i = 1, structure._cid - 1 do
         local id1, id2 = structure._connections[i]:match("^(.-)%s*,%s*(.-)$")
-        masterSave:addConnectionRaw(tostring(id1+masterBID-1)..','..tostring(id2+masterBID-1))
+        masterSave:addConnectionRaw(tostring(id1 + masterBID - 1) .. "," .. tostring(id2 + masterBID - 1))
     end
-    masterBID=masterBID+structure._bid-1
+
+    masterBID = masterBID + structure._bid - 1
+
     function structure:getInput(name)
-        if self._inputs[name]==nil then error("{cm2s.lua} Error: no input manifest found with name "..name) end
+        if self._inputs[name] == nil then
+            error("{cm2s.lua} Error: no input manifest found with name " .. name)
+        end
         return self._inputs[name]
     end
+
     function structure:getOutput(name)
-        if self._outputs[name]==nil then error("{cm2s.lua} Error: no output manifest found with name "..name) end
+        if self._outputs[name] == nil then
+            error("{cm2s.lua} Error: no output manifest found with name " .. name)
+        end
         return self._outputs[name]
     end
+
     return structure
 end
-function cm2s:connect(inp,out)
-    if #inp~=#out then error("{cm2s.lua} Error: input and output bits arent the same") end
-    for i=1,#inp do
-        local inpOrigin=structureOrigins[inp._hash]
-        local outOrigin=structureOrigins[out._hash]
-        
-        masterSave:addConnection(tostring(tonumber(inp[i]+inpOrigin-1)),tostring(tonumber(out[i]+outOrigin-1)))
+
+--wizardry
+function cm2s:connect(inp, out)
+    if #inp ~= #out then
+        error("{cm2s.lua} Error: input and output bits arent the same")
+    end
+    for i = 1, #inp do
+        local inpOrigin = structureOrigins[inp._hash]
+        local outOrigin = structureOrigins[out._hash]
+
+        masterSave:addConnection(tostring(tonumber(inp[i] + inpOrigin - 1)), tostring(tonumber(out[i] + outOrigin - 1)))
     end
 end
 function cm2s:retrieve()
     return masterSave
 end
 
-return cm2s 
+return cm2s
