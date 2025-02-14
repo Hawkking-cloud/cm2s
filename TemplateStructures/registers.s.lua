b6e005d460b0e7610ad9356768c2717b53aaf29d
Hawkking-cloud <59718035+Hawkking-cloud@users.noreply.github.com>
2025-02-14 13:45:11 -0600

    structureZ fixes
    - changed code to use official structureZ 
    - changed all z values to use correct structureZ

--- TemplateStructures/registers.s.lua
+++ TemplateStructures/registers.s.lua
@@ -1,8 +1,8 @@
 local dep = require("cm2sDEP")
 local structure = {}
 -- this is a structure object
---node: when modeling circuits of a structure file, make sure to use STRUCTZ for your z values of every block or the blocks will overlap with others
-local STRUCTZ = structure._zID
+--node: when modeling circuits of a structure file, make sure to use structureZ for your z values of every block or the blocks will overlap with others
+local structureZ = 0
 local currentY = 0
 function structure:manual()
     -- dep:manStringify() can be substituted with your own manual format
@@ -34,6 +34,7 @@ function structure:manual()
 end
 
 function structure:lite(bits)
+    structureZ = structure._structureZ
     local save = dep:newSave(1 + (bits * 4), bits * 5)
 
     --manifesting the inputs and outputs
@@ -42,32 +43,33 @@ function structure:lite(bits)
 
     save:manifestOutput("data")
 
-    local write = save:addBlock(15, -2, currentY, STRUCTZ)
+    local write = save:addBlock(15, -2, currentY, structureZ)
     save:addInput(write, "write")
 
     for i = 1, bits do
-        local input = save:addBlock(15, i, currentY, STRUCTZ)
+        local input = save:addBlock(15, i, currentY, structureZ)
         save:addInput(input, "data")
 
-        local xorGate = save:addBlock(3, i, currentY + 1, STRUCTZ)
+        local xorGate = save:addBlock(3, i, currentY + 1, structureZ)
         save:addConnection(input, xorGate)
 
-        local andGate = save:addBlock(1, i, currentY + 2, STRUCTZ)
+        local andGate = save:addBlock(1, i, currentY + 2, structureZ)
         save:addConnection(write, andGate)
         save:addConnection(xorGate, andGate)
 
-        local output = save:addBlock(5, i, currentY + 3, STRUCTZ)
+        local output = save:addBlock(5, i, currentY + 3, structureZ)
         save:addOutput(output, "data")
 
         save:addConnection(output, xorGate)
         save:addConnection(andGate, output)
     end
 
-    save.zI = STRUCTZ
+    save.zI = structureZ
     currentY = currentY + 4 -- make sure to increment this by the max Y of your blocks or youll have overlapping blocks
     return save
 end
 function structure:normal(bits)
+    structureZ = structure._structureZ
     local save = dep:newSave(1 + (bits * 4), bits * 5)
 
     save:manifestInput("write")
@@ -76,27 +78,27 @@ function structure:normal(bits)
 
     save:manifestOutput("data")
 
-    local write = save:addBlock(15, -2, currentY, STRUCTZ)
+    local write = save:addBlock(15, -2, currentY, structureZ)
     save:addInput(write, "write")
 
-    local clear = save:addBlock(15, -2, currentY + 1, STRUCTZ)
+    local clear = save:addBlock(15, -2, currentY + 1, structureZ)
     save:addInput(clear, "clear")
 
     for i = 1, bits do
-        local input = save:addBlock(15, i, currentY, STRUCTZ)
+        local input = save:addBlock(15, i, currentY, structureZ)
         save:addInput(input, "data")
 
-        local xorGate = save:addBlock(3, i, currentY + 1, STRUCTZ)
+        local xorGate = save:addBlock(3, i, currentY + 1, structureZ)
         save:addConnection(input, xorGate)
 
-        local andGate = save:addBlock(1, i, currentY + 2, STRUCTZ)
+        local andGate = save:addBlock(1, i, currentY + 2, structureZ)
         save:addConnection(write, andGate)
         save:addConnection(xorGate, andGate)
 
-        local andGate2 = save:addBlock(1, i, currentY + 2, STRUCTZ)
+        local andGate2 = save:addBlock(1, i, currentY + 2, structureZ)
         save:addConnection(clear, andGate2)
 
-        local output = save:addBlock(5, i, currentY + 3, STRUCTZ)
+        local output = save:addBlock(5, i, currentY + 3, structureZ)
         save:addOutput(output, "data")
 
         save:addConnection(output, xorGate)
@@ -105,7 +107,6 @@ function structure:normal(bits)
         save:addConnection(andGate2, output)
     end
 
-    save.zI = STRUCTZ
     currentY = currentY + 5
     return save
 end
